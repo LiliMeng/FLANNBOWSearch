33e359d366ea68d128ef5b94a5a8aca432c4ca08
LiliMeng <lilimeng1103@gmail.com>
2016-02-12 22:05:05 -0800

    Update imageScore.cpp

--- imageScore.cpp
+++ imageScore.cpp
@@ -6,31 +6,22 @@
 
 using namespace std;
 
-
-
-
-int main()
+void search_image(int imgNum, vector<int> images, multimap<int, float> &imageScore)
 {
-    multimap<int, int> imageScore;
-
-    int imgNum=50;
-
     for(int i=0; i<imgNum; i++)
     {
-        imageScore.insert(pair<int,int>(i+1,0));
+        imageScore.insert(pair<int,float>(i+1,0.0));
     }
 
     cout<<imageScore.size()<<endl;
-
-    vector<int> image={1,12,13,23,12,13,16,12,18,19};
 
     int score=0;
 
     for(auto ite=imageScore.begin(); ite!=imageScore.end(); ite++)
     {
-        for(int i=0; i<image.size(); i++)
+        for(int i=0; i<images.size(); i++)
         {
-            if((*ite).first==image[i])
+            if((*ite).first==images[i])
             {
                 (*ite).second++;
             }
@@ -38,10 +29,26 @@ int main()
         }
 
         ///normalize
-        float normalized_score=(float)(*ite).second/(float)image.size();
+        float normalized_score=(float)(*ite).second/(float)images.size();
+
+        (*ite).second=normalized_score;
         cout.precision(5);
         cout<<(*ite).first<<" "<<(*ite).second<<" "<<normalized_score<<endl;
     }
+
+
+}
+
+
+int main()
+{
+    multimap<int, float> imageScore;
+
+    int imgNum=50;
+
+    vector<int> images={1,12,13,23,12,13,16,12,18,19};
+
+    search_image(imgNum, images, imageScore);
 
     return 0;
 
