a4b9f4b0f108cd9a464506fe366f5bbf27b84687
LiliMeng <lilimeng1103@gmail.com>
2016-02-13 13:49:28 -0800

    Update mainFeb13.cpp

--- mainFeb13.cpp
+++ mainFeb13.cpp
@@ -1,67 +1,131 @@
-#ifndef VOCABULARY_H
-#define VOCABULARY_H
-
-#include <cassert>
-#include <map>
-#include <vector>
-#include <opencv2/opencv.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/features2d/features2d.hpp>
-#include <opencv2/nonfree/features2d.hpp>
-#include <opencv2/calib3d/calib3d.hpp> // for homography
+#include <iostream>
+#include <ctime>
+#include <Vocabulary.h>
 
 using namespace std;
 
-struct RankedScore {
-    int imageIndex;
-    float imageScore;
-};
+using namespace cv;
 
-struct by_number {
-    bool operator()(RankedScore const &left, RankedScore const &right) {
-        return left.imageScore > right.imageScore;
-    }
-};
-
-
-class Vocabulary
+int main()
 {
-    public:
-        Vocabulary(int imgNum);
-        virtual ~Vocabulary();
-    void clear();
-    multimap<int, int> addWords(const cv::Mat & descriptors, int objectIndex, bool incremenal);
-	void update();
-	void search(const cv::Mat & descriptors, cv::Mat & results, cv::Mat & dists, int k);
-	int size() const
-	{
-        return indexedDescriptors_.rows + notIndexedDescriptors_.rows;
-	}
+    int imgNum=300;
 
-	const multimap<int, int> & wordToObjects() const
-	{
-        return wordToObjects_;
-	}
+    vector<Mat>  imgVec;
+    imgVec.resize(imgNum);
 
-	int clustering(const cv::Mat& features, cv::Mat& centers);
-	void search_descriptors(const cv::Mat & descriptors, cv::Mat & results, cv::Mat & dists, int k);
-	void search_image(const cv::Mat & descriptors, cv::Mat & results, cv::Mat & dists, int k, int imgNum, vector<unsigned int> labels, vector<int> images, multimap<int, int> &imageScore, vector<RankedScore> &rankedScore);
+    vector<string> nameVec;
+    nameVec.resize(imgNum);
 
-    void queryDistribution(const cv::Mat & features, vector<float> & distribution) const;
-    void computeLikelihood(const cv::Mat & features, vector<float> &likelihood) const;
+    vector<vector<KeyPoint> > keyPointsVec;
+    keyPointsVec.resize(imgNum);
 
-    cv::flann::SearchParams getFlannSearchParams();
-    cvflann::flann_distance_t getFlannDistanceType();
-    static cv::flann::IndexParams * createFlannIndexParams(int index);
+    vector<Mat> descriptorsVec;
+    descriptorsVec.resize(imgNum);
 
-    int nLabel_;
+    for(int i=0; i<imgNum; i++)
+    {
+        char fileName[1024] ={NULL};
 
-    cv::flann::Index flannIndex_;
-    cv::Mat indexedDescriptors_;
-    cv::Mat notIndexedDescriptors_;
-    multimap<int, int> wordToObjects_;
-    vector<int> notIndexedWordIds_;
-};
+        sprintf(fileName, "/home/lili/workspace/SLAM/vocabTree/Lip6IndoorDataSet/Images/lip6kennedy_bigdoubleloop_%06d.ppm", i);
 
-#endif // VOCABULARY_H
+        nameVec[i]=string(fileName);
+
+        imgVec[i]=imread(nameVec[i], CV_LOAD_IMAGE_GRAYSCALE);
+    }
+
+    //-- Step 1: Detect the keypoints using SURF Detector
+    int minHessian = 400;
+
+    SurfFeatureDetector detector(minHessian);
+
+    SurfDescriptorExtractor extractor;
+
+    vector<unsigned int> labels;
+    for(int i=0; i<imgNum; i++)
+    {
+        detector.detect(imgVec[i], keyPointsVec[i]);
+
+        extractor.compute(imgVec[i], keyPointsVec[i], descriptorsVec[i]);
+        for(int j = 0; j<descriptorsVec[i].rows; j++)
+        {
+            labels.push_back(i);
+        }
+    }
+
+    Mat all_descriptors;
+
+    for(int i = 0; i<descriptorsVec.size(); i++)
+    {
+        all_descriptors.push_back(descriptorsVec[i]);
+    }
+
+    assert(labels.size() == all_descriptors.rows);
+    cout<<"all_descriptors.rows "<<all_descriptors.rows<<endl;
+    cout<<"hahha1 "<<endl;
+    Vocabulary vocab(imgNum);
+
+    vocab.indexedDescriptors_ = all_descriptors;
+
+
+    vector<KeyPoint> newKeypoints;
+    Mat newDescriptors;
+
+    ///add new image to the randomized kd tree
+    {
+        string newImageName="/home/lili/workspace/SLAM/vocabTree/Lip6IndoorDataSet/Images/lip6kennedy_bigdoubleloop_000350.ppm";
+        Mat newImg=imread(newImageName, CV_LOAD_IMAGE_GRAYSCALE);
+        detector.detect(newImg, newKeypoints);
+        extractor.compute(newImg, newKeypoints, newDescriptors);
+        cout<<"newDescriptors.rows: "<<newDescriptors.rows<<endl;
+    }
+
+    vocab.notIndexedDescriptors_ = newDescriptors;
+
+    ///clustering
+    int clustersNum;
+    Mat clusters(15000,64,CV_32F);
+    //Mat float_all_descriptors;
+
+
+    clustersNum=vocab.clustering(all_descriptors, clusters);
+    cout<<"clustersNum  "<<clustersNum<<endl;
+
+    ///flann build tree
+    clock_t begin1 = clock();
+    vocab.update();
+    clock_t end1 = clock();
+    double buildTree_time = double(end1 - begin1) / CLOCKS_PER_SEC;
+    cout.precision(5);
+    cout<<"buildTree time "<<buildTree_time<<endl;
+
+
+    cout<<"hahha2 "<<endl;
+    vector<KeyPoint> queryKeypoints;
+    Mat queryDescriptors;
+
+    ///QueryImage
+    {
+        string queryImageName="/home/lili/workspace/SLAM/vocabTree/Lip6IndoorDataSet/Images/lip6kennedy_bigdoubleloop_000381.ppm";
+        Mat queryImg=imread(queryImageName, CV_LOAD_IMAGE_GRAYSCALE);
+        detector.detect(queryImg, queryKeypoints);
+        extractor.compute(queryImg, queryKeypoints, queryDescriptors);
+        cout<<"queryDescriptors.rows: "<<queryDescriptors.rows<<endl;
+    }
+
+
+
+    Mat indices;
+    Mat results;
+    Mat dists;
+    int k=1;
+    vector<int> imageLabelsVec;
+
+    multimap<int, int> imageScore;
+    vector<RankedScore> rankedScore;
+    vocab.search_image(queryDescriptors, indices, dists, k, imgNum, labels, imageLabelsVec, imageScore, rankedScore);
+
+    vector<double> likelihood;
+    vocab.computeLikelihood(imgNum, rankedScore, likelihood);
+
+    return 0;
+}
